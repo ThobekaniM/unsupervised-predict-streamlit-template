ebe25de760309655fa114914c73b3664b7d76a03
Rikus Kruger <rikusetiennekruger@gmail.com>
2021-07-21 17:18:14 +0200

    commit

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -131,7 +131,8 @@ def main():
                     f'col_1': col2,
                     f'col_2': col3}
 
-        movies = [None] * 3
+        movies_titles = [None] * 3
+        movies_ids = [None] * 3
         genres = [None] * 3
         years = [None] * 3
         
@@ -145,28 +146,9 @@ def main():
             genre_movies_temp = movies_df[movies_df['movieId'].isin(genre_movie_id_temp)]
             genre_year_movies_temp = genre_movies_temp[genre_movies_temp['release_year'] == years[col]]
 
-            movies[col] = col_dict[key].selectbox(f'Select movie {col + 1}:', genre_year_movies_temp['title'].to_list())
-
-
-            # genre_2 = col2.selectbox('2nd Movie Genre', genre_list)
-            # year_2 = col2.selectbox('2nd Movie Year', year_list)
-
-            # genre_movie_id_2 = genres[genres["Genre"] == genre_2]["movieId"]
-            # genre_movies_2 = movies_df[movies_df['movieId'].isin(genre_movie_id_2)]
-            # genre_year_movies_2 = genre_movies_2[genre_movies_2['release_year'] == year_2]
-
-            # movie_2 = col2.selectbox('Second Option', genre_year_movies_2['title'].to_list())
-
-            # genre_3 = col3.selectbox('3rd Movie Genre', genre_list)
-            # year_3 = col3.selectbox('3rd Movie Year', year_list)
-
-            # genre_movie_id_3 = genres[genres["Genre"] == genre_3]["movieId"]
-            # genre_movies_3 = movies_df[movies_df['movieId'].isin(genre_movie_id_3)]
-            # genre_year_movies_3 = genre_movies_3[genre_movies_3['release_year'] == year_3]
-            # movie_3 = col3.selectbox('Third Option', genre_year_movies_3['title'].to_list())
-
-
-        fav_movies = movies
+            movies_titles[col] = col_dict[key].selectbox(f'Select movie {col + 1}:', genre_year_movies_temp['title'].to_list())
+            movies_ids[col] = movies_df[movies_df['title'] == movies_titles[col]]["movieId"].values
+            
 
 
         # Perform top-10 movie recommendation generation
@@ -174,7 +156,7 @@ def main():
             if col1.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
-                        top_recommendations = content_model(movie_list=fav_movies,
+                        top_recommendations = content_model(movies_titles,
                                                             top_n=10)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
@@ -188,7 +170,7 @@ def main():
             if col1.button("Recommend"):
                 try:
                     with st.spinner('Crunching the numbers...'):
-                        top_recommendations = collab_model(movie_list=fav_movies,
+                        top_recommendations = collab_model(movie_list=movies_ids,
                                                            top_n=10)
                     st.title("We think you'll like:")
                     for i,j in enumerate(top_recommendations):
@@ -201,8 +183,10 @@ def main():
         
         if sys == '1':
             recommend = col1.button("Recommend")
-            if recommend:    
-                predicted_movies_ids = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+            if recommend:
+                predicted_movies_ids = content_model(movies_titles, 10)
+                st.write(predicted_movies_ids)
+                # predicted_movies_ids = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
                 new_col1, new_col2, new_col3, new_col4, new_col5 = st.beta_columns(5)
 
                 new_col_dict = {f'col_0': new_col1,
@@ -213,6 +197,8 @@ def main():
 
                 for i, id in enumerate(predicted_movies_ids[:5]):
                     
+
+                    new_col_dict[f'col_{i}'].write(id)
                     title_dict = {f'movie_{id}_title': movies_df[movies_df["movieId"] == id]["title"].values[0]}
                     director_dict = {f'movie_{id}_director': imdb_df[imdb_df["movieId"] == id]["director"].values[0]}
                     cast_dict = {f'movie_{id}_cast': imdb_df[imdb_df["movieId"] == id]["title_cast"].values[0]}
@@ -228,75 +214,7 @@ def main():
 
                     new_col_dict[f'col_{i}'].subheader("Cast:")
                     for member in cast_dict[f'movie_{id}_cast']:
-                        new_col_dict[f'col_{i}'].write(f" - {member}\n")
-
-
-                if new_col_dict[f'col_4'].button("Next"):
-                    for i, id in enumerate(predicted_movies_ids[5:10]):
-                        
-                        title_dict = {f'movie_{id}_title': movies_df[movies_df["movieId"] == id]["title"].values[0]}
-                        director_dict = {f'movie_{id}_director': imdb_df[imdb_df["movieId"] == id]["director"].values[0]}
-                        cast_dict = {f'movie_{id}_cast': imdb_df[imdb_df["movieId"] == id]["title_cast"].values[0]}
-
-                        search_keyword = title_dict[f'movie_{id}_title'].replace(' ', '+')
-                        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword + "+trailer")
-                        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                        new_col_dict[f'col_{i}'].video("https://www.youtube.com/watch?v=" + video_ids[0])
-
-
-                        new_col_dict[f'col_{i}'].title(title_dict[f'movie_{id}_title'])
-                        new_col_dict[f'col_{i}'].empty()
-                        new_col_dict[f'col_{i}'].subheader("Director:")
-                        new_col_dict[f'col_{i}'].write(f" - {director_dict[f'movie_{id}_director']}")
-
-                        new_col_dict[f'col_{i}'].subheader("Cast:")
-                        for member in cast_dict[f'movie_{id}_cast']:
-                            new_col_dict[f'col_{i}'].write(f" - {member}\n")
-
-
-
-                                     
-                # image_2 = new_col2.image('resources/imgs/1.jpg')
-                # # cast_button_2 = new_col2.button('Cast2')
-                # # if cast_button_2:
-                # new_col2.subheader("Director:")
-                # new_col2.write(f" - {movie_2_director.values[0]}")
-                # new_col2.subheader("Cast:")   
-                # for cast in movie_2_cast:
-                #     for member in cast:
-                #         new_col2.write(f" - {member}\n")
-
-                # image_3 = new_col3.image('resources/imgs/1.jpg')
-                # # cast_button_3 = new_col3.button('Cast3')
-                # # if cast_button_3:
-
-                # new_col3.subheader("Director:")
-                # new_col3.write(f" - {movie_3_director.values[0]}")
-                # new_col3.subheader("Cast:")
-                # for cast in movie_3_cast:
-                #     for member in cast:
-                #         new_col3.write(f" - {member}\n")
-
-
-                # image_4 = new_col4.image('resources/imgs/1.jpg')
-                # # cast_button_4 = new_col4.button('Cast4')
-                # # if cast_button_4:
-                # new_col4.subheader("Director:")
-                # new_col4.write(f" - {movie_4_director.values[0]}")
-                # new_col4.subheader("Cast:")
-                # for cast in movie_4_cast:
-                #     for member in cast:
-                #         new_col4.write(f" - {member}\n")               
-                
-                # image_5 = new_col5.image('resources/imgs/1.jpg')
-                # # cast_button_5 = new_col5.button('Cast5')
-                # # if cast_button_5:\
-                # new_col5.subheader("Director:")
-                # new_col5.write(f" - {movie_5_director.values[0]}")
-                # new_col5.subheader("Cast:")
-                # for cast in movie_5_cast:
-                #     for member in cast:
-                #         new_col5.write(f" - {member}\n")      
+                        new_col_dict[f'col_{i}'].write(f" - {member}\n")   
 
 
 
